1069734bc314da56be331c260e395692b7a25605
Edward <edward.azarevich@gmail.com>
2025-12-16 23:16:38 +0300

    add list for nav

--- src/component/Nav.jsx
+++ src/component/Nav.jsx
@@ -1,5 +1,16 @@
 const NavBar = () => {
-    return <nav>Nav</nav>
+    return (
+        <nav>
+            <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Menu</li>
+                <li>Reservations</li>
+                <li>Home</li>
+                <li>Order online</li>
+                <li>Login</li>
+            </ul>
+        </nav>);
 }
 
 export default NavBar;
