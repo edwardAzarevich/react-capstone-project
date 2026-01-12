67e49687020b955c45462fecd40e0d9f07aa0e19
Edward <edward.azarevich@gmail.com>
2026-01-13 01:09:04 +0300

    add rationg component

---
+++ src/component/rating/Rating.jsx
@@ -0,0 +1,15 @@
+import './Rating.css';
+
+const Rating = () => {
+    return (
+        <div className="rating-result">
+            <span className="active"></span>
+            <span className="active"></span>
+            <span className="active"></span>
+            <span className="active"></span>
+            <span className="active"></span>
+        </div>
+    );
+};
+
+export default Rating;
