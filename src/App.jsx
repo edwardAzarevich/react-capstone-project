b60c042224177fdbcf866b8f26af20ecfc934844
Edward <edward.azarevich@gmail.com>
2026-01-19 15:44:55 +0300

    add booking page and Route for navigation

--- src/App.jsx
+++ src/App.jsx
@@ -5,28 +5,40 @@ import CallToActionMain from './component/callToAction/CallToActionMain';
 import Special from './component/special/Special';
 import Testimonials from './component/customersSay/Testimonials';
 import About from './component/about/About';
+import BookingPage from './component/bookingPage/BookingPage';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
+
+function HomePage() {
+  return (
+    <div className='root'>
+      <header>
+        <Header />
+      </header>
+      <div className='info-background'>
+        <CallToActionMain />
+      </div>
+      <div>
+        <Special />
+      </div>
+      <Testimonials />
+      <About />
+      <footer className='info-background'>
+        <Footer />
+      </footer>
+    </div>
+  );
+}
 
 function App() {
-    return (
-        <div className='root'>
-            <header>
-                <Header />
-            </header>
-            <div className='info-background'>
-                <CallToActionMain />
-            </div>
-            <div>
-                <Special />
-            </div>
-            {<Testimonials />}
-            <About />
-
-            <footer className='info-background'>
-                <Footer />
-            </footer>
-        </div>
-    );
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<HomePage />}></Route>
+        <Route path='/booking' element={<BookingPage />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
